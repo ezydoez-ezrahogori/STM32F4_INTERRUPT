0540af886731651beb94cc6003c53d51c293e402
Bukiezrah <79102300+ezydoez-ezrahogori@users.noreply.github.com>
2023-04-23 12:41:50 +0300

    timer_interrupt

---
+++ 19_Timer_Interrupt/Src/tim.c
@@ -0,0 +1,55 @@
+/*
+ * tim.c
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: Ezrah Buki
+ */
+#include<stm32f4xx.h>
+
+#define TIM2EN      (1U<<0)
+#define CR1_CEN     (1U<<0)
+#define   DIER_UIE  (1U<<0)
+
+
+
+void tim2_1hz_init(void)
+{
+	/*Enable clock access to tm2*/
+	RCC->AHB1ENR |= TIM2EN;
+	/*Set Prescaler value*/
+	TIM2->PSC = 1600-1 ;// 16 000 000/1600 =100000
+	/*set auto-reload value*/
+    TIM2->ARR = 10000-1; // 10000/10000 = 1
+	/*Clear counter*/
+    TIM2->CNT = 0;
+	/*Enable counter*/
+    TIM2->CR1 = TIM2EN;
+}
+
+
+void tim2_1hz_interrupt_init(void)
+{
+	/*Enable clock access to tm2*/
+	RCC->AHB1ENR |= TIM2EN;
+	/*Set Prescaler value*/
+	TIM2->PSC = 1600-1 ;// 16 000 000/1600 =100000
+	/*set auto-reload value*/
+    TIM2->ARR = 10000-1; // 10000/10000 = 1
+	/*Clear counter*/
+    TIM2->CNT = 0;
+	/*Enable timer*/
+    TIM2->CR1 = TIM2EN;
+
+    /*Enable TIM interrupt*/
+    TIM2->DIER |= DIER_UIE;
+
+    /*Enable TIM interrupt in NVIC*/
+    NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+
+
+
+
+
+
